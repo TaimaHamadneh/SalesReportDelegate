0ecef2bc8324231fa55f6606b2d7dc7c89e41d26
TaimaHamadenh <taimanizar45@gmail.com>
2025-10-19 15:34:25 +0300

    Add project files.

---
+++ C#Delegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using C_Delegate;
+
+var emps = new Employee[]
+{
+new Employee{ Id = 1, Name = "Taima H", Gender = "F", TotalSales = 65000m},
+new Employee { Id = 2, Name = "Reem S", Gender = "F", TotalSales = 50000m },
+new Employee { Id = 3, Name = "Suzan B", Gender = "F", TotalSales = 65000m },
+new Employee { Id = 4, Name = "Sara C", Gender = "F", TotalSales = 40000m },
+new Employee { Id = 5, Name = "Saleh A", Gender = "M", TotalSales = 42000m },
+new Employee { Id = 6, Name = "Rateb C", Gender = "M", TotalSales = 30000m },
+new Employee { Id = 7, Name = "Abeer I", Gender = "F", TotalSales = 16000m },
+new Employee { Id = 8, Name = "Marwan M", Gender = "M", TotalSales = 15000m }
+};
+ 
+var report = new Report();
+report.ProcessEmployee(emps, "Employees with Sales $60,000+",(Employee e) => e.TotalSales > 60000m );
+report.ProcessEmployee(emps, "Employees with Sales Between $30,000 and $60,000",(Employee e) => 30000 <= e.TotalSales && e.TotalSales < 60000m);
+report.ProcessEmployee(emps, "Employees with Sales Less Than $30,000",(Employee e) => e.TotalSales < 30000m);
+ 
---
+++ C#Delegate/Report.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Delegate
+{
+    public class Report
+    {
+
+        public delegate bool EligibleSales(Employee e);
+
+
+        public void ProcessEmployee(Employee[] employees, string title, EligibleSales isIllegible)
+        {
+            Console.WriteLine(title);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            foreach (var e in employees)
+            {
+                if (isIllegible(e))
+                {
+                    Console.WriteLine($"{e.Id} | {e.Name} | {e.Gender} | ${e.TotalSales} ");
+                }
+            }
+            Console.WriteLine("\n\n");
+
+        }
+    }
+}
+    
